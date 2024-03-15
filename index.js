8977e6df3c217cc64a58cb55dde906f0d4583c89
Hollis Crowder <hollis.crowder@gmail.com>
2024-03-15 14:39:18 -0700

    complete sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+// Add user info
+function submitData(name, email) {
+  const formData = {
+    name: `${name}`,
+    email: `${email}`,
+  };
+
+  const objectConfiguration = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", objectConfiguration)
+    .then((response) => response.json())
+    .then((user) => {
+      const id = document.createElement("p");
+      id.textContent = user.id;
+      document.body.append(id);
+    })
+    .catch((error) => {
+      const errorMessage = document.createElement("p");
+      errorMessage.textContent = error.message;
+      document.body.append(errorMessage);
+    });
+}
